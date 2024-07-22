0d40c8ff0b96417d4c98249e489bd8e2d3d12909
Kangmin Kim <76634341+amazon7737@users.noreply.github.com>
2024-07-22 22:53:41 +0900

    feat: StreetPicture 컨트롤러 추가

---
+++ src/main/java/org/example/tabara/streetpicture/controller/StreetPicture.java
@@ -0,0 +1,11 @@
+package org.example.tabara.streetpicture.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class StreetPicture {
+
+}
+
