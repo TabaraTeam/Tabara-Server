37ae0ff5a569fe0e46bd2a37608cd5dbb25cc1c5
gdhhkkl <lee040319ww@naver.com>
2024-07-22 22:57:07 +0900

    fix:파일명 수정

--- src/main/java/org/example/tabara/streetpicture/controller/StreetPictureController.java
+++ src/main/java/org/example/tabara/streetpicture/controller/StreetPictureController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-public class StreetPicture {
+public class StreetPictureController {
 
 }
 
