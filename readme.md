1f79387229d3f74f8c9ebe183e976316321fd12a
Kangmin Kim <76634341+amazon7737@users.noreply.github.com>
2024-07-22 20:29:04 +0900

    init: 프로젝트 생성

---
+++ readme.md
@@ -0,0 +1,9 @@
+## Tabara_Server
+
+### Naming
+
+### Feature
+
+### Swagger
+
+### 구현 및 추가 설명
---
+++ src/main/java/org/example/tabara/TabaraApplication.java
@@ -0,0 +1,13 @@
+package org.example.tabara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TabaraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TabaraApplication.class, args);
+    }
+
+}
