8cfd6e61726c9ff2f9a94284fb2b964caa21eb80
Kangmin Kim <76634341+amazon7737@users.noreply.github.com>
2024-08-09 21:47:27 +0900

    feat: 더미데이터 파일 작성

---
+++ src/main/resources/import.sql
@@ -0,0 +1,6 @@
+-- 위치 좌표 더미데이터
+
+insert into User(email, nickname, password, username) values ("amazon7737@gmail.com", "amazon", "1234", "amazon1234");
+
+insert into Park(latitude, longitude, description, name) values (35.169167981481664, 128.97354876665054, "삼락생태공원입니다.", "삼락생태공원");
+
