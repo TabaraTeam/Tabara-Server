007fcf13a860d09a2affe28c20a020f5cc9c0d92
gdhhkkl <lee040319ww@naver.com>
2024-08-10 20:26:57 +0900

    fix: Comtroller 내용 수정했습니다.

--- src/main/java/org/example/tabara/notice/controller/NoticeController.java
+++ src/main/java/org/example/tabara/notice/controller/NoticeController.java
@@ -1,10 +1,36 @@
 package org.example.tabara.notice.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.example.tabara.notice.domain.Notice;
+import org.example.tabara.notice.dto.NoticeDto;
+import org.example.tabara.notice.repository.NoticeRepository;
+import org.example.tabara.notice.service.NoticeDtoService;
+import org.example.tabara.notice.service.NoticeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 public class NoticeController {
+
+    private final NoticeService noticeService;
+
+
+
+    @GetMapping("/notice")
+    public List<NoticeDto> getUserDto() {
+        List<NoticeDto> noti = noticeService.finAll();
+        return noti;
+    }
+
+
+
+
+
+
 
 }
