31a775551c831c9620b52d4eb6a4ad435a1c6550
gdhhkkl <lee040319ww@naver.com>
2024-08-10 22:35:10 +0900

    delete: 주석 제거

--- src/main/java/org/example/tabara/notice/dto/NoticeDto.java
+++ src/main/java/org/example/tabara/notice/dto/NoticeDto.java
@@ -11,11 +11,6 @@ public class NoticeDto {
     private  String content;
 
 
-/*
-* @NoArgsConstructor : 파라미터가 없는 디폴트 생성자를 생성
-@AllArgsConstructor : 모든 필드 값을 파라미터로 받는 생성자를 생성
-@RequiredArgsConstructor : final이나 @NonNull으로 선언된 필드만을 파라미터로 받는 생성자를 생성
- @DATA : 클래스 멤버 변수의 Getter/Setter 메서드를 구현해준다
-* */
+
 
 }
