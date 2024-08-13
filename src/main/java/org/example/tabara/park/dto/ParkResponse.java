c2edf2d1a7dfb62417d1cebd62209046f49c664c
WhatJH <030226jihyun@naver.com>
2024-08-13 23:37:30 +0900

    feat : ParkResponse 파일 작성

---
+++ src/main/java/org/example/tabara/park/dto/ParkResponse.java
@@ -0,0 +1,19 @@
+package org.example.tabara.park.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import org.example.tabara.park.domain.Park;
+
+@Getter
+@Builder
+//@AllArgsConstructors
+public class ParkResponse {
+
+    private String name;
+
+    private double latitude;
+
+    private double longitude;
+
+
+}
